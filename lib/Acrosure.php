766dc9ee75823613f114dcf75998a62c169a0a23
M.Charattrakool <m.charattrakool@gmail.com>
2018-10-11 21:51:48 +0700

    create manager subnamespace

--- lib/Acrosure.php
+++ lib/Acrosure.php
@@ -1,11 +1,11 @@
 <?php
 
 require_once dirname(__FILE__).'/api-resource.php';
-require_once dirname(__FILE__).'/application-manager.php';
-require_once dirname(__FILE__).'/product-manager.php';
-require_once dirname(__FILE__).'/team-manager.php';
-require_once dirname(__FILE__).'/data-manager.php';
-require_once dirname(__FILE__).'/policy-manager.php';
+require_once dirname(__FILE__).'/manager/application.php';
+require_once dirname(__FILE__).'/manager/product.php';
+require_once dirname(__FILE__).'/manager/team.php';
+require_once dirname(__FILE__).'/manager/data.php';
+require_once dirname(__FILE__).'/manager/policy.php';
 require_once dirname(__FILE__).'/client.php';
 
 ?>
